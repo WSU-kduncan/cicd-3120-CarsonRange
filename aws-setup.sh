1c95c4aa2c907a94cb750f92cdd4efed85498f9d
CarsonRange <77698947+CarsonRange@users.noreply.github.com>
2022-04-13 15:17:32 -0400

    Update aws-setup.sh

--- aws-setup.sh
+++ aws-setup.sh
@@ -2,8 +2,7 @@
 sudo apt install update -y && sudo apt install upgrade -y && sudo apt install git -y && sudo apt install docker -y && sudo apt install docker.io -y && sudo apt install apache2 -y
 
 #add current user to docker group
-$user=`whoami`
-sudo usermod -aG docker $user
+sudo usermod -aG docker ubuntu
 
 #chmod scripts
 chmod 744 /website/start.sh /website/restart.sh
