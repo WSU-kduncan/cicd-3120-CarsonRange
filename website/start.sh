f3a8bd0674e31a81f000f8cbe7a4b1ae730fbef7
CarsonRange <77698947+CarsonRange@users.noreply.github.com>
2022-04-13 14:21:43 -0400

    Update start.sh

--- website/start.sh
+++ website/start.sh
@@ -1,3 +1,3 @@
-cd /home/carson/ceg3120/cicd-3120-CarsonRange/website
+cd ~/ceg3120/cicd-3120-CarsonRange/website
 docker build -t my-apache2 .
 docker run -dit --name my-running-app -p 8080:80 my-apache2
