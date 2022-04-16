a6c26bd1a2d197b3b561e38db7aa2635e8840713
CarsonRange <range.6@wright.edu>
2022-04-16 15:39:26 -0400

    updated aws setup script

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,5 +1,7 @@
 #install stuff
-sudo apt install update -y && sudo apt install upgrade -y && sudo apt install git -y && sudo apt install docker -y && sudo apt install docker.io -y && sudo apt install apache2 -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt install git -y && sudo apt install docker -y && sudo apt install docker.io -y && sudo apt install apache2 -y
 
 #add current user to docker group
 sudo usermod -aG docker ubuntu
+
+sudo reboot
