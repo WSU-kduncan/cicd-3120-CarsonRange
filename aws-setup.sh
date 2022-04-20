d4f550a8ca79bd89fc65ad1aa3d342be7601c6c8
CarsonRange <range.6@wright.edu>
2022-04-20 15:08:18 -0400

    added webhooks port to sc and updated aws-setup

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,5 +1,5 @@
 #install stuff
-sudo apt update -y && sudo apt upgrade -y && sudo apt install git -y && sudo apt install docker -y && sudo apt install docker.io -y && sudo apt install apache2 -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt install git -y && sudo apt install docker -y && sudo apt install docker.io -y && sudo apt install apache2 -y && sudo apt install webhook
 
 #add current user to docker group
 sudo usermod -aG docker ubuntu
