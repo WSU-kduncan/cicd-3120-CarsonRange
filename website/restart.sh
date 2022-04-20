9f14458bc0763aa3fcb291075f6f421154739211
range <range.6@wright.edu>
2022-04-20 18:12:27 +0000

    fixed restart script

--- website/restart.sh
+++ website/restart.sh
@@ -1,8 +1,9 @@
-docker pull range6/project-5:latest
-
 #kill and rm
 docker stop my-running-app
 docker rm my-running-app
+
+#pull from docker
+docker pull range6/project-5:latest
 
 #start
 docker pull range6/project-5:latest
