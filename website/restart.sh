c2095f916169e77e2f998d58e95db8577bc73c57
range <range.6@wright.edu>
2022-04-21 20:15:44 +0000

    update

--- website/restart.sh
+++ website/restart.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/bash
+
 #kill and rm
 docker image prune -f
 docker stop my-running-app
 docker rm my-running-app
 
 #pull from docker
-docker build -t my-apache2 /home/ubuntu/cicd-3120-CarsonRange/website
+#docker build -t my-apache2 /home/ubuntu/cicd-3120-CarsonRange/website
 
 #start
 docker run -dit --name my-running-app -p 80:80 my-apache2
